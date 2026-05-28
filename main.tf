4f0432d7e25879ba7032148fa6cfe6dd52752387
2kita9 <likhitha.sivapuram@gmail.com>
2026-05-28 10:21:07 +0530

    version error resolved

--- main.tf
+++ main.tf
@@ -4,11 +4,10 @@ terraform {
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = "~> 6.0"
+      version = ">= 6.0"
     }
   }
 }
-
 provider "aws" {
   region = var.aws_region
 }
